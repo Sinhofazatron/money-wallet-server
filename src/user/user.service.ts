ee6129809c3d1590430917598d88707977e404b3
Sitnikov Dmitry <betani.elvin@mail.ru>
2023-11-03 12:43:52 +0300

    Feat: Initial commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,45 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as argon2 from "argon2";
+import { JwtService } from '@nestjs/jwt'
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {
+
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const existUser = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email
+      }
+    })
+    if (existUser) throw new BadRequestException('Такой Email уже существует!')
+    const user = await this.userRepository.save({
+      email: createUserDto.email,
+      password: await argon2.hash(createUserDto.password),
+    })
+
+    const token = this.jwtService.sign({ email: createUserDto.email })
+
+    return { user, token };
+  }
+
+
+  async findOne(email: string) {
+    return await this.userRepository.findOne({
+      where: {
+        email,
+      }
+    })
+  }
+
+
+}
